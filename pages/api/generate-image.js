4a9daab189fa7f55251dcc5664fd8f097ec9e41a
ottotto0 <dayaogongsong@gmail.com>
2026-07-07 21:17:28 +0900

    Update model version in generate-image API

--- pages/api/generate-image.js
+++ pages/api/generate-image.js
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
         }
 
         const result = await client.predict("/infer", [
-            "v160",             // model
+            "v140",             // model
             prompt,             // prompt
             "masterpiece, best quality, fine details", // quality_prompt
             "blurry, low quality, watermark, monochrome, text", // negative_prompt
